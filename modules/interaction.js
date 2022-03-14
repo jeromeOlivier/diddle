d68d9edfb869d6e7e26fa11a15a80b1b4b5502ae
jeromeOlivier <jerome.olivier@icloud.com>
2022-03-13 21:37:05 -0400

    refactoring logic, using the html as a pseudo database

--- modules/interaction.js
+++ modules/interaction.js
@@ -1,8 +1,9 @@
 import * as filter from './filters.js';
 import * as draw from './draw.js';
+import * as gen from './generators.js'
 
 const words = [];
-const word = [];
+const ltr = {'idx': 0, 'ltr': '', 'sta': ''}
 
 function start() {
   // event listener for user keyboard interaction (keys)
@@ -31,83 +32,103 @@ function start() {
     }))
 }
 
-// HELPER FUNCTIONS ***
+// HELPER FUNCTIONS ------------------------------------------------------------
 // check conditions after a key or button event is triggered
 function checkEvent(val) {
   if (canAdd(val)) return addLetter(val);
-  if (canSubmit(val)) return submitWord(word);
+  if (canSubmit(val)) return submitWord();
   if (canDelete(val)) return deleteLetter();
+}
+
+function constructWord() {
+  const squares = document.querySelector('div.row:last-child').childNodes;
+  // copy the statuses of each square to the letter objects of the word
+  const word = [];
+  squares.forEach(square => {
+    const ltr = {};
+    const index = square.getAttribute('data-idx');
+    const letter = square.getAttribute('data-ltr');
+    const status = square.getAttribute('data-sta');
+    // if there's no letter, don't crerate an object
+    if (letter !== ' ') {
+      ltr['idx'] = letter === undefined ? ' ' : index;
+      ltr['ltr'] = letter === undefined ? ' ' : letter;
+      ltr['sta'] = status === undefined ? ' ' : status;
+      word.push(ltr);
+    }
+  });
+  return word;
 }
 
 // add letter to the word and print it to the screen
 function addLetter(letter) {
   // draw letter on screen
-  const position = `[data-row="${words.length}"] [data-idx="${word.length}"]`;
+  const index = gen.next(currentIndex());
+  const position = `[data-row="${words.length}"] [data-idx="${index}"]`;
   const cell = document.querySelector(position);
   cell.setAttribute('data-ltr', letter);
   cell.textContent = letter.toUpperCase();
-  // add letter to word
-  const formedLetter = formLetter(letter)
-  return word.push(formedLetter);
-}
-
-// each letter has 3 attributes: position, letter and status (default is square)
-function formLetter(letter) {
-  return {'idx': word.length, 'ltr': letter, 'sta': 'blank'}
-}
-
-// if word is complete, analyze it (returning suggestions) and draw more squares
-function submitWord(word) {
-  if (containsNoBlanks(word)) {
-    // lock current row
-    // const position = `[data-row="${words.length}"]`;
-    // const row = document.querySelector(position);
-    // row.setAttribute('data-row-sta', 'locked');
-    if (words.length < 6) draw.squares();
-    // process words
-
-    words.push(word);
-    filter.analyzeWord(words);
-    word.forEach(() => word.pop());
-  } else {
-    console.log("peanuts")
-  }
 }
 
 // delete letter from the word and remove it from the screen
 function deleteLetter() {
-  word.pop();
-  const position = `[data-row="${words.length}"] [data-idx="${word.length}"]`;
+  const index = currentIndex();
+  const position = `[data-row="${words.length}"] [data-idx="${index}"]`;
+  console.log(position)
   const cell = document.querySelector(position);
   cell.textContent = "";
   cell.setAttribute('data-ltr', ' ');
   cell.setAttribute('data-sta', 'blank');
 }
 
+// if word is complete, analyze it (returning suggestions) and draw more squares
+function submitWord() {
+  const word = constructWord();
+  if (!containsBlanks(word)) {
+    // process word
+    words.push(word);
+    filter.analyzeWord(words);
+    // draw a fresh row of squares if there are less than 6
+    if (words.length < 6) draw.squares();
+  }
+}
+
+function currentIndex() {
+  let index = undefined;
+  const squares = document.querySelector('div.row:last-child').childNodes;
+  squares.forEach(square => {
+    const idx = Number(square.getAttribute('data-idx'));
+    const letter = square.getAttribute('data-ltr');
+    if (letter !== ' ') index = idx;
+  });
+  return index === undefined ? -1 : index;
+}
+
 // BOOLEANS
 function canAdd(val) {
+  const word = constructWord();
   return val >= 'a' && val <= 'z' && word.length < 5 && words.length < 6;
 }
 
 function canSubmit(val) {
+  const word = constructWord();
   return val === 'Enter' && word.length === 5 && words.length < 6;
 }
 
 function canDelete(val) {
-  return val === 'Delete' || val === 'Backspace' && word.length > 0;
+  const word = constructWord();
+  return (val === 'Delete' || val === 'Backspace') && word.length > 0;
 }
 
-function containsNoBlanks(word) {
-  const blanks = [];
+function containsBlanks(word) {
+  const blanks = []
   word.forEach(letter => {
     console.log(letter.sta);
-    // if (letter.sta === 'blank') blanks.push(letter)
+    if (letter.sta === 'blank') blanks.push(letter)
   });
-  console.log(blanks.length)
-  return !blanks;
+  return blanks;
 }
 
 export {
   start,
-  word,
 }
